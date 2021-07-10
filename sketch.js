a3760920bf26e79b61fcc0f2c57d20063bda309f
Subhrajyoti2608 <86191223+Subhrajyoti2608@users.noreply.github.com>
2021-07-10 20:11:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,150 @@
-var jaxon, jaxon_running, edges
-var path
-var pathImage
-var coin
-var coinImg
+var bow , arrow,  scene;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+
+var score=0;
+
 function preload(){
-  //pre-load images
-  jaxon_running = loadAnimation("Runner-1.png","Runner-2.png")
-  pathImage = loadImage("path.png")
-  coinImg = loadImage("coin.png")
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
+  
 }
 
-function setup(){
-  createCanvas(400,400);
-  //create sprites here
+
+
+function setup() {
+  createCanvas(400, 400);
   
-  path = createSprite(200,200)
-  path.addImage(pathImage);
-  path.y = path.height/2
-  path.scale=1.5
-  path.velocityY=5
- 
- 
-  right_boundary = createSprite(0,800.50,800)
-  right_boundary.visible=false
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
   
-  left_boundary= createSprite(400,0,50,800)
-  left_boundary.visible=false
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
   
-  jaxon = createSprite(200,400,100,50)
-  jaxon.addAnimation("Running", jaxon_running)
-  jaxon.scale=0.2
-  coin = createSprite(70,300,20,20)
-  coin.addImage(coinImg)
-  coin.scale=0.8
+   score = 0    
 }
 
 function draw() {
-  background(180);
-  path.velocityY=5
+ background(0);
+  // moving ground
+    scene.velocityX = -3 
 
- 
- 
- jaxon.x=World.mouseX
- jaxon.y=World.mouseY
- edges = createEdgeSprites()
- jaxon.collide(right_boundary)
- jaxon.collide(left_boundary)
- if(path.y > 400){
-  path.y = path.height/2;  
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
   }
+   
+  //creating continous enemies
+  var select_balloon = Math.round(random(1,4));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
+    } else if (select_balloon == 2) {
+      greenBalloon();
+    } else if (select_balloon == 3) {
+      blueBalloon();
+    } else {
+      pinkBalloon();
+    }
+  }  
+    
   drawSprites();
+  text("Score: "+ score, 300,50);
+}
 
+
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+  arrowGroup.add(arrow)
+}
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+  redB.add(red)
+}
+
+function blueBalloon() {
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+  blueB.add(blue)
+}
+
+function greenBalloon() {
+  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
+  greenB.add(green)
+}
+
+function pinkBalloon() {
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1
+  pinkB.add(pink)
+}
+redB= new Group();
+blueB= new Group();
+greenB= new Group();
+pinkB= new Group();
+arrowGroup= new Group();
+
+
+if (arrowGroup.isTouching(redB)){
+  redB.destroyEach();
+  arrowGroup.destroyEach();
+  score=score+1
+}
+
+if (arrowGroup.isTouching(blueB)){
+  blueB.destroyEach();
+  arrowGroup.destroyEach();
+  score=score+3
+}
+
+if (arrowGroup.isTouching(greenB)){
+  greenB.destroyEach();
+  arrowGroup.destroyEach();
+  score=score+5
+}
+
+if (arrowGroup.isTouching(pinkB)){
+  pinkB.destroyEach();
+  arrowGroup.destroyEach();
+  score=score+2
 }
